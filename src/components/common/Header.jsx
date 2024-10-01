a3a90f0246053ba97bd0f69c64ae12848e19ec91
khi8194 <khi8194@gmail.com>
2024-10-01 17:23:08 +0900

    기존 a태그를 Link컴포넌트로 대체, Link컴포넌트를 통한 라우터 이동

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,11 +2,14 @@
 // import { FaInstagram } from 'react-icons/fa';
 // import { FaEnvelope } from 'react-icons/fa';
 import { FaYoutube, FaInstagram, FaEnvelope } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+//React의 Link컴포넌트를 활용해서 메뉴이동처리 하면
+//불필요한 서버요청 없이 미리 한번에 불러온 컴포넌트를 실시간으로 클라이언트단에서 변경처리 가능
 
 export default function Header() {
 	return (
 		<header className='header'>
-			<h1>
+			{/* <h1>
 				<a href='/'>ALPACO</a>
 			</h1>
 
@@ -26,6 +29,29 @@ export default function Header() {
 					</li>
 					<li>
 						<a href='/posts'>POSTS</a>
+					</li>
+				</ul> */}
+
+			<h1>
+				<Link to={'/'}>ALPACO</Link>
+			</h1>
+
+			<nav>
+				<ul className='gnb'>
+					<li>
+						<Link to={'/members'}>MEMBERS</Link>
+					</li>
+					<li>
+						<Link to={'/gallery'}>GALLERY</Link>
+					</li>
+					<li>
+						<Link to={'/youtube'}>YOUTUBE</Link>
+					</li>
+					<li>
+						<Link to={'/contact'}>CONTACT</Link>
+					</li>
+					<li>
+						<Link to={'/posts'}>POSTS</Link>
 					</li>
 				</ul>
 
