2941f73244b168c2709417bc6d4ae92feded42ff
khi8194 <khi8194@gmail.com>
2024-10-02 16:22:31 +0900

    ceoBox, 멤버페이지 스타일링, members 페이지 반응형 처리

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -1,38 +1,9 @@
 import Layout from '../common/Layout';
-// import Title from '../common/Title';
 import memberData from '../../data/memberData';
 
 export default function Members() {
-	console.log(memberData);
 	return (
 		<Layout title={'MEMBERS'}>
-			{/* <p>Members Page contents come here.</p> */}
-
-			{/* 
-			<div className="pic">
-				<img src="이미지" alt="" />
-			</div>
-			<div className="txt">
-				<h2>사람 이름</h2>
-				<p>직책</p>
-			</div> */}
-
-			{/* 미션: 위의 구조로 반복 출력하도록 배열과 .map 함수를 이용해서 구현 */}
-
-			{/* {memberData.map((member, idx) => {
-				return (
-					<article key={idx}>
-						<div className='pic'>
-							<img src={'/' + member.pic} alt={member.name} />
-						</div>
-						<div className='txt'>
-							<h2>{member.name}</h2>
-							<p>{member.position}</p>
-						</div>
-					</article>
-				);
-			})} */}
-
 			{/* 첫번째 데이터만 뽑아서 출력 */}
 			<article className='ceoBox'>
 				<div className='txt'>
@@ -43,7 +14,16 @@ export default function Members() {
 					<img src={'/' + memberData[0].pic} alt={memberData[0].name} />
 				</div>
 			</article>
-			<article className='memberListbox'>
+
+			<article className='memberListBox'>
+				<div className='titBox'>
+					<h2>Our Team Member</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, iste accusantium! Eum corrupti voluptates
+						natus! Harum dolorum reprehenderit modi nostrum?
+					</p>
+				</div>
+
 				<ul>
 					{memberData.map((member, idx) => {
 						//첫번째 순번의 데이터가 아닐때에만 반복출력
@@ -62,11 +42,16 @@ export default function Members() {
 						}
 					})}
 				</ul>
+
+				<div className='descBox'>
+					<h2>Lorem ipsum dolor sit.</h2>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. A esse cupiditate, vitae deleniti repellat
+						explicabo sit, corrupti beatae dicta, nulla optio corporis alias. Perferendis quidem sapiente minima,
+						quisquam inventore soluta.
+					</p>
+				</div>
 			</article>
 		</Layout>
 	);
 }
-
-//미션
-//위의 7개 배열 중에서 첫번째 데이터만 .ceoBox안쪽 출력
-//첫번쨰를 제외한 나머지 6개 데이터만 기존 반복문 구문 안에서 출력
--- src/data/memberData.js
+++ src/data/memberData.js
@@ -3,9 +3,9 @@ export default [
 	{ name: 'Paul Yoon', pic: 'paul.jpg', position: 'CFO' },
 	{ name: 'Emily Chung', pic: 'emily.jpg', position: 'CTO' },
 	{ name: 'Peter Smith', pic: 'peter.jpg', position: 'UI Designer' },
-	{ name: 'Julia Min', pic: 'julia.jpg', position: 'Fronted Dev' },
+	{ name: 'Julia Min', pic: 'julia.jpg', position: 'Frontend Dev' },
 	{ name: 'Michael Hall', pic: 'michael.jpg', position: 'Backend Dev' },
-	{ name: 'Emma Smith', pic: 'emma.jpg', position: 'Prject Manager' }
+	{ name: 'Emma Smith', pic: 'emma.jpg', position: 'Project Manager' }
 ];
 
 //JSX : 화면에 요소를 출력하기 위한 JSX를 반환해주는 컴포넌트 함수 파일
