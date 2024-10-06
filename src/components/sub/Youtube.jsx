ecb6ed7007207d0415f78b97c1820ac0ad9cabd3
khi8194 <khi8194@gmail.com>
2024-10-07 01:15:33 +0900

    커스텀 훅 제작 방법 주석 정리

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -1,18 +1,20 @@
 import { useState, useEffect } from 'react';
 import Layout from '../common/Layout';
 import Pic from '../common/Pic';
+import useShortenText from '../../../hooks/useShortenText';
 
 export default function Youtube() {
+	const shortenText = useShortenText();
 	const [Vids, setVids] = useState([]);
 
 	const fetchYoutube = () => {
 		// const api_key = 'AIzaSyCKLA8E45lLrWn_7MlJL692LpwsZ9mwa_4';
+		shortenText('David');
 		const api_key = import.meta.env.VITE_YOUTUBE_API;
 		const pid = 'PL7dKBcBdt1leSwaAYMfi9B9GkbLK_A_oh';
 		const num = 10;
 		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
 
-		// useEffect(() => {
 		fetch(url)
 			.then(data => data.json())
 			.then(json => {
@@ -31,33 +33,12 @@ export default function Youtube() {
 				return (
 					<article key={idx}>
 						<Pic className='thumb' src={vid.snippet.thumbnails.high.url} />
-						{/* <h3>{vid.snippet.title}</h3> */}
 						<h3>{vid.snippet.title.length >= 60 ? vid.snippet.title.substr(0, 60) + '...' : vid.snippet.title}</h3>
 						<p>{vid.snippet.description}</p>
 						<span>{vid.snippet.publishedAt}</span>
 					</article>
 				);
 			})}
-
-			{/* 썸네일을 가져오기 어려울 경우 썼던 코드 */}
-			{/* {Vids.length > 0 ? (
-				Vids.map((vid, idx) => {
-					// thumbnails와 high 객체가 존재하는지 확인
-					const thumbnail = vid.snippet.thumbnails?.high?.url;
-
-					return (
-						<article key={idx}> */}
-			{/* thumbnail이 존재하는지 조건부 렌더링 */}
-			{/* {thumbnail ? <Pic className='thumb' src={thumbnail} /> : <p>No thumbnail available</p>}
-							<h3>{vid.snippet.title}</h3>
-							<p>{vid.snippet.description}</p>
-							<span>{new Date(vid.snippet.publishedAt).toLocaleDateString()}</span>
-						</article>
-					);
-				})
-			) : (
-				<p>Loading videos...</p>
-			)} */}
 		</Layout>
 	);
 }
