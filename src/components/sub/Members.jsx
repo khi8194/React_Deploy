34a7f86fedd12321fd73198d010c6319eeb567a9
khi8194 <khi8194@gmail.com>
2024-10-06 22:09:39 +0900

    참조형 자료를 state에 담고 변경할때의 주의사항

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -5,15 +5,11 @@ import Pic from '../common/Pic';
 export default function Members() {
 	return (
 		<Layout title={'MEMBERS'}>
-			{/* 첫번째 데이터만 뽑아서 출력 */}
 			<article className='ceoBox'>
 				<div className='txt'>
 					<h2>{memberData[0].name}</h2>
 					<p>{memberData[0].position}</p>
 				</div>
-				{/* <div className='pic'>
-					<img src={'/' + memberData[0].pic} alt={memberData[0].name} />
-				</div> */}
 				<Pic className='pic' src={'/' + memberData[0].pic} shadow />
 			</article>
 
@@ -28,14 +24,9 @@ export default function Members() {
 
 				<ul>
 					{memberData.map((member, idx) => {
-						//첫번째 순번의 데이터가 아닐때에만 반복출력
 						if (idx !== 0) {
 							return (
 								<li key={idx}>
-									{/* <div className='pic'>
-										<img src={'/' + member.pic} alt={member.name} />
-									</div> */}
-									{/* 이미지 컴포넌트 호출후 src에 이미지 url값 전달, pic클래스에는 이미지의 크기정도만 지정 */}
 									<Pic src={member.pic} className='pic' shadow={true} />
 									<div className='txt'>
 										<h2>{member.name}</h2>
