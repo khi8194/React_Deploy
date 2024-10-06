ba9636e0700ab2b4c1d88dccfa3da84538b8ef52
khi8194 <khi8194@gmail.com>
2024-10-07 01:08:52 +0900

    유튜브 제목 말줄임 처리

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -5,19 +5,24 @@ import Pic from '../common/Pic';
 export default function Youtube() {
 	const [Vids, setVids] = useState([]);
 
-	// const api_key = 'AIzaSyCKLA8E45lLrWn_7MlJL692LpwsZ9mwa_4';
-	const api_key = import.meta.env.VITE_YOUTUBE_API;
-	const pid = 'PL7dKBcBdt1leSwaAYMfi9B9GkbLK_A_oh';
-	const num = 10;
-	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
+	const fetchYoutube = () => {
+		// const api_key = 'AIzaSyCKLA8E45lLrWn_7MlJL692LpwsZ9mwa_4';
+		const api_key = import.meta.env.VITE_YOUTUBE_API;
+		const pid = 'PL7dKBcBdt1leSwaAYMfi9B9GkbLK_A_oh';
+		const num = 10;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
 
-	useEffect(() => {
+		// useEffect(() => {
 		fetch(url)
 			.then(data => data.json())
 			.then(json => {
 				console.log(json.items);
 				setVids(json.items);
 			});
+	};
+
+	useEffect(() => {
+		fetchYoutube();
 	}, []);
 
 	return (
@@ -26,13 +31,15 @@ export default function Youtube() {
 				return (
 					<article key={idx}>
 						<Pic className='thumb' src={vid.snippet.thumbnails.high.url} />
-						<h3>{vid.snippet.title}</h3>
+						{/* <h3>{vid.snippet.title}</h3> */}
+						<h3>{vid.snippet.title.length >= 60 ? vid.snippet.title.substr(0, 60) + '...' : vid.snippet.title}</h3>
 						<p>{vid.snippet.description}</p>
 						<span>{vid.snippet.publishedAt}</span>
 					</article>
 				);
 			})}
 
+			{/* 썸네일을 가져오기 어려울 경우 썼던 코드 */}
 			{/* {Vids.length > 0 ? (
 				Vids.map((vid, idx) => {
 					// thumbnails와 high 객체가 존재하는지 확인
