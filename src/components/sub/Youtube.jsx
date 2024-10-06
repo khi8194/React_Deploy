3e11282bb6926d2b8063b14935e0b19eba8fc9a1
khi8194 <khi8194@gmail.com>
2024-10-07 01:50:45 +0900

    다이나믹 라우터를 통한 유튜브 고유 id정보 상세페이지에 전달

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -3,6 +3,7 @@ import Layout from '../common/Layout';
 import Pic from '../common/Pic';
 import useShortenText from '../../../hooks/useShortenText';
 import useCombineText from '../../../hooks/useCombineText';
+import { Link } from 'react-router-dom';
 
 export default function Youtube() {
 	const shortenText = useShortenText();
@@ -34,10 +35,10 @@ export default function Youtube() {
 			{Vids.map((vid, idx) => {
 				return (
 					<article key={idx}>
-						{/* <Pic className='thumb' src={vid.snippet.thumbnails.high.url} /> */}
-						<h3>{shortenText(vid.snippet.title, 60)}</h3>
-						{/* <p>{shortenText(vid.snippet.description, 150)}</p>
-						<span>{combineText(vid.snippet.publishedAt.split('T')[0], '-', '.')}</span> */}
+						{/* <h3>{shortenText(vid.snippet.title, 60)}</h3> */}
+						<h3>
+							<Link to={'/youtube/' + vid.id}>{shortenText(vid.snippet.title, 60)}</Link>
+						</h3>
 						<div className='text'>
 							<p>{shortenText(vid.snippet.description, 150)}</p>
 							<span>{combineText(vid.snippet.publishedAt.split('T')[0], '-', '.')}</span>
