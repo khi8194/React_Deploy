ec014e9ca3db1d83b59ee27f0c3b67a09031f8ae
khi8194 <khi8194@gmail.com>
2024-10-07 17:10:48 +0900

    모바일 메뉴 토글 미션 (선택사항)

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,8 +5,6 @@ export default function Header() {
 	const gnbArr = ['members', 'gallery', 'youtube', 'contact', 'posts'];
 	const snsArr = [FaEnvelope, FaInstagram, FaYoutube];
 
-	// const path = useLocation();
-	// console.log(path);		//고유키값 path이름값
 	const { pathname } = useLocation();
 	console.log(pathname);
 
@@ -17,27 +15,8 @@ export default function Header() {
 			</h1>
 
 			<nav>
-				{/* <ul className='gnb'>
-					<li>
-						<Link to={'/members'}>MEMBERS</Link>
-					</li>
-					<li>
-						<Link to={'/gallery'}>GALLERY</Link>
-					</li>
-					<li>
-						<Link to={'/youtube'}>YOUTUBE</Link>
-					</li>
-					<li>
-						<Link to={'/contact'}>CONTACT</Link>
-					</li>
-					<li>
-						<Link to={'/posts'}>POSTS</Link>
-					</li>
-				</ul> */}
 				<ul className='gnb'>
 					{gnbArr.map((data, idx) => {
-						//미션
-						//위의 pathname값을 활용해서 gnb의 li 요소에 on클래스를 붙여서 메뉴명 활성화 로직 구현
 						return (
 							<li key={idx} className={pathname === '/' + data ? 'on' : ''}>
 								<Link to={'/' + data}>{data.toUpperCase()}</Link>
@@ -47,23 +26,10 @@ export default function Header() {
 				</ul>
 
 				<ul className='sns'>
-					{/* <li>
-						<FaYoutube />
-					</li>
-					<li>
-						<FaInstagram />
-					</li>
-					<li>
-						<FaEnvelope />
-					</li> */}
-					{/* 화살표함수 특성상 JSX반환시 {return}문은 생략 가능 */}
 					{snsArr.map((Data, idx) => {
-						// return (
 						<li key={idx}>
-							{/* snsArr에서 반복을 돌면서 Data파라미터로 전달되는 각각의 객체는 컴포넌트 함수 아래와같이 JSX문으로 호출 가능 이때 컴포넌트 규칙에 따라 파리미터 명도 대문자로 시작해서 호출 */}
 							<Data />
 						</li>;
-						// );
 					})}
 				</ul>
 			</nav>
@@ -72,3 +38,10 @@ export default function Header() {
 		</header>
 	);
 }
+
+/*
+	미션 -
+	Header 컴포넌트에서 모바일 메뉴 토글 버튼 클릭시 
+	common / MobileMenu컴포넌트를 토글 형식으로 열리고 닫기도록 처리
+	Modal.jsx작업 흐름을 참고해서 제작
+*/
