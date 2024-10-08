52bc5c208f0e4e3d6c76e7e8ae9be495b0dee87f
khi8194 <khi8194@gmail.com>
2024-10-08 13:20:03 +0900

    텍스트모션 오류 해결

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -5,13 +5,12 @@ import { useEffect, useRef } from 'react';
 
 export default function Layout({ title, children }) {
 	const ref_title = useRef(null);
-	//커스텀훅으로 핸들러함수 안쪽에서 호출할 수 있는 실제 사용가능한 함수 반환받음
 	const splitText = useSplitText();
 	const { pathname } = useLocation();
-
 	const isDetail = pathname.includes('/youtube/');
 
 	useEffect(() => {
+		//훅 자체적으로 참조객체 요소 활성화 처리
 		splitText(ref_title);
 	}, []);
 
@@ -31,12 +30,3 @@ export default function Layout({ title, children }) {
 		</main>
 	);
 }
-
-/*
-	motion 컴포넌트에서 자주 쓰는 스타일 속성
-	x: 가로축 이동 (숫자, 퍼센트는 문자열 처리)
-	y: 세로축 이동
-	scale:확대
-	rotate: 회전
-	opacity: 투명도
-*/
--- src/hooks/useSplitText.js
+++ src/hooks/useSplitText.js
@@ -1,12 +1,3 @@
-/*
-- 훅에서 함수를 리턴하는 이유 
-- 여러 컴포넌트에서 동시다발적으로 호출해야 되는데
-- 특정 컴포넌트에서 호출될때 필요한 고유 정보값을 계속 유지해야됨
-- 클로저 환경 필요
-- 함수 안쪽에 지역변수를 만들고 그 지역변수를 활용하는 함수를 리턴하면 렉시컬 스코프환경이 설정됨
-- 렉시컬 스코프 환경 : 안쪽의 지역변수가 계속 값이 유지되는 현상
-*/
-
 export default function useSplitText() {
 	//내부적으로 current값을 직접 추출하기때문에 호출시 참조객체명만 전달
 	return ref => {
@@ -17,10 +8,23 @@ export default function useSplitText() {
 
 		//문자열을 반복돌면서 동적으로 <span>으로 감싸면서 문자열 쌓아나감
 		for (let letter of text) {
-			tags += `<span>${letter}</span>`;
+			tags += `<span style='display:inline-block; transition-duration:0.5s;'>${letter}</span>`;
 		}
 
 		//tag문자열이 완성되면 ref참조 요소 안쪽에 변경된 문자열 DOM구조 바꿔치기
 		ref.current.innerHTML = tags;
+		//매번 호출하는 부모 컴포넌트에서 on을 추가하는 구문이 번거로우므로
+		//훅 자체적으로 해당 요소 자체에 on을 추가
+		setTimeout(() => {
+			ref.current.classList.add('on');
+		}, 100);
 	};
 }
+/*
+  setTimeout의 delay값을 0으로만 줘도 
+  해당 코드는 무조건 ref.current.innerHTML = tags; 실행된뒤 호출됨 (동기화됨)
+  그럼에도 불구하고 100이라는 지연시간을 준 이유는
+  동기화 시점이 innerHTML로 동적 요소를 넣는 호출 시점일 뿐
+  실제 동적으로 DOM이 완료된 이후를 보장하진 않기 때문에
+  물리적으로 실제 돔으로 변환될 약간의 시간을 확보하기 위함
+*/
