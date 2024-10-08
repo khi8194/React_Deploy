9fa3b485897faf411e5b146cc91b382d904e2aad
khi8194 <khi8194@gmail.com>
2024-10-08 16:18:04 +0900

    기본 모션 컴포넌트 수정

--- src/components/common/MaskText.jsx
+++ src/components/common/MaskText.jsx
@@ -1,19 +1,67 @@
-import { useEffect, useRef } from 'react';
+// import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
 export default function MaskText({ children }) {
-	console.log('mask');
-	const ref_frame = useRef(null);
+	// const [Mounted, setMounted] = useState(false);
+	//기본 스타일 객체
+	const frameStyle = {
+		fontSize: '1.2rem',
+		fontFamily: 'orbitron',
+		color: '#555',
+		display: 'inline-block',
+		position: 'relative',
+		overflow: 'hidden',
+		marginBottom: 80
+	};
+	// const conStyle = {
+	// 	opacity: 0,
+	// 	transitionDuration: '0.1s',
+	// 	transitionDelay: '0.3s'
+	// };
+	const maskStyle = {
+		width: '100%',
+		height: '100%',
+		position: 'absolute',
+		top: 0,
+		backgroundColor: '#555'
+	};
 
-	useEffect(() => {
-		setTimeout(() => {
-			ref_frame.current.classList.add('on');
-		}, 100);
-	}, []);
+	// //con, mask의 활성화 스타일 객체
+	// const conStyleActive = { ...conStyle, opacity: 1 };
+	// const maskStyleActive = { ...maskStyle, left: '100%' };
+
+	// useEffect(() => {
+	// 	setTimeout(() => {
+	// 		setMounted(true);
+	// 	}, 1000);
+	// }, []);
+
+	/*
+	return (
+		<div className='slogan' style={frameStyle}>
+			<span style={Mounted ? conStyleActive : conStyle}>{children}</span>*/
+	{
+		/* <div className='mask'></div> */
+	} /*
+					<div style={Mounted ? maskStyleActive : maskStyle}></div>
+				</div>
+			);*/
 
 	return (
-		<div className='slogan' ref={ref_frame}>
-			<span>{children}</span>
-			<div className='mask'></div>
+		<div style={frameStyle}>
+			<motion.span
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0, transition: { delay: 0 } }}
+				transition={{ duration: 0.01, delay: 0.3 }}>
+				{children}
+			</motion.span>
+
+			<motion.div
+				style={maskStyle}
+				initial={{ x: '-100%' }}
+				animate={{ x: '100%' }}
+				transition={{ duration: 0.6 }}></motion.div>
 		</div>
 	);
 }
