6276c3f09923ea998a1c0a98ff1caace39885f1f
khi8194 <khi8194@gmail.com>
2024-10-08 17:48:42 +0900

    박스 마스크 모션 처리 완료

--- src/components/common/MaskBox.jsx
+++ src/components/common/MaskBox.jsx
@@ -1,37 +1,43 @@
+import { useLocation } from 'react-router-dom';
+import useSplitText from '../../hooks/useSplitText';
+import { useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
-export default function MaskBox({ children, duration, delay, color, style }) {
-	//기본 스타일 객체
-	const frameStyle = {
-		display: 'inline-block',
-		position: 'relative',
-		overflow: 'hidden'
-	};
-	const maskStyle = {
-		width: '100%',
-		height: '100%',
-		position: 'absolute',
-		top: 0,
-		backgroundColor: color
-	};
+import MaskText from './MaskText';
+
+export default function Layout({ title, children }) {
+	const ref_title = useRef(null);
+	const splitText = useSplitText();
+	const { pathname } = useLocation();
+	const isDetail = pathname.includes('/youtube/');
+
+	useEffect(() => {
+		splitText(ref_title, { interval: 0.1 });
+	}, [splitText]);
+	//useEffect에 의존성 배열에 특정 값을 등록하라고 뜨는 경우
+	//해당 컴포넌트자체적으로 제어되지 않는 요소가 useEffect안쪽에서 활용되고 있을때 등록하라는 권고 사항 출력
+	//해결 방법: 등록 처리 (잘못등록하면 재귀적호출 되면서 무한호출 문제)
+	//무한호출시 해결방법 : useMemo, useCallback등의 메모이제이션 훅을 이용해서 강제로 메모리에 등록후 사용
+
 	return (
-		<div style={{ ...frameStyle, ...style }}>
-			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				exit={{ opacity: 0, transition: { delay: 0 } }}
-				transition={{ duration: 0.01, delay: duration / 2 + delay }}>
+		<main className={isDetail ? 'detail' : title.toLowerCase()}>
+			<h1 ref={ref_title}>{title}</h1>
+
+			<MaskText duration={1} delay={0} color={'#000'}>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ad?
+			</MaskText>
+			<br />
+
+			<MaskText duration={0.6} delay={1} color={'#555'}>
+				Lorem ipsum dolor
+			</MaskText>
+
+			<motion.section
+				initial={{ opacity: 0, y: 200 }}
+				animate={{ opacity: 1, y: 0 }}
+				exit={{ opacity: 0, y: 200, transition: { delay: 0 } }}
+				transition={{ duration: 1, delay: 0.7, ease: 'linear' }}>
 				{children}
-			</motion.div>
-			<motion.div
-				style={maskStyle}
-				initial={{ x: '-101%' }}
-				animate={{ x: '101%' }}
-				transition={{ duration, delay }}></motion.div>
-		</div>
+			</motion.section>
+		</main>
 	);
 }
-/*
-  미션 (5시 30분 까지)
-  - MaskBox.jsx 라는 새로운 컴포넌트 생성
-  - 이미지나 그룹덩어리의 박스요소에 마스크모션 처리
-*/
