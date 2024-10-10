b4aab54dd4a84458a799a8528d4e7049bbb81783
khi8194 <khi8194@gmail.com>
2024-10-10 14:48:49 +0900

    서브 페이지내 특정 영역만 모션을 공통으로 처리하고 싶을떄의 방안 주석 정리

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import MaskText from './MaskText';
+// import { motion } from 'framer-motion';
+// import MaskText from './MaskText';
 import Mask from './Mask';
 import SplitText from './SplitText';
 
@@ -14,22 +14,23 @@ export default function Layout({ title, children }) {
 				<SplitText delay={0.5}>{title}</SplitText>
 				<br />
 
-				<MaskText delay={1} color={'#444'} style={{ fontSize: 20, fontFamily: 'arial' }}>
+				{/* <MaskText delay={1} color={'#444'} style={{ fontSize: 20, fontFamily: 'arial' }}>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ad?
 				</MaskText>
 				<br />
 				<MaskText delay={1.5} color={'#444'} style={{ marginBottom: 120 }}>
 					Lorem ipsum dolor
-				</MaskText>
+				</MaskText> */}
 
-				{/* 순서5. 두번째 줄 마스크 모션 끝날떄쯤 전체 컨텐츠 영상 위쪽으로 페이드인 */}
-				<motion.section
+				<section>{children}</section>
+				{/* <motion.section
 					initial={{ opacity: 0, y: 200 }}
 					animate={{ opacity: 1, y: 0 }}
 					exit={{ opacity: 0, y: 200, transition: { delay: 0 } }}
-					transition={{ duration: 1, delay: 1.5, ease: 'linear' }}>
+					// transition={{ duration: 1, delay: 1.5, ease: 'linear' }}>
+					transition={{ duration: 1, delay: 0.5, ease: 'linear' }}>
 					{children}
-				</motion.section>
+				</motion.section> */}
 			</main>
 
 			<Mask duration={0.5} delay={0} style={{ position: 'fixed' }} />
--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -3,13 +3,7 @@ import memberData from '../../data/memberData';
 import Pic from '../common/Pic';
 import { useRef, useState } from 'react';
 import MaskBox from '../common/MaskBox';
-
-/*
-참조객체에 가상돔을 담아 활용하는 패턴
-1. useRef로 빈 참조객체 생성
-2. 원하는 가상돔요소 (JSX El) ref속성으로 참조객체 연결
-3. 참조객체명.current로 해당요소를 가져와서 제어
-*/
+import MaskText from '../common/MaskText';
 
 export default function Members() {
 	console.log('Member rendered!!');
@@ -29,6 +23,12 @@ export default function Members() {
 
 	return (
 		<Layout title={'MEMBERS'}>
+			<MaskText delay={1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ad?</MaskText>
+			<br />
+			<MaskText delay={1.5} style={{ marginBottom: 80 }}>
+				Lorem ipsum dolor
+			</MaskText>
+
 			<article className='ceoBox'>
 				<div className='txt'>
 					<h2>{memberData[0].name}</h2>
@@ -83,17 +83,12 @@ export default function Members() {
 }
 
 /*
-state
--컴포넌트가 재랜더링 되더라도 값이 사라지지 않고 계속 유지 (이전값을 기억하면서 재활용가능)
--해당값이 변경되면 자동으로 컴포넌트가 재랜더링됨
--사용예: JSX뱐호와 관련된 모든값은 state에 담아줌, 서버데이터, 모달을 열기위한 불린값, 목록클릭시 변경되야 되는 순서값
-
-useRef
--컴포넌트가 재랜더링 되더라도 값이 사라지지 않고 계속 유지 (이전값을 기억하면서 재활용가능)
--해당값이 변경되더라도 컴포넌트를 재랜더링시키지 않음
--사용예1: 화면의 랜더링과 직접적인 연관은 없지만 로직활용시 유지되야되는 값
--사용예2: 브라우저 리사이즈시 갱신되야되는 브라우저의 폭, 스크롤시 갱신해야 되는 현재 스크롤 위치
-
-컴포넌트 함수가 재랜더링(재호출)되더라도 State와 useRef의 값을 기억할 수 있는 이유 
--자바스크립트 lexical scope가 클로저(closure)환경을 기반으로 하고 있기 때문 
+	미션
+	- MaskText컴포넌트 아래쪽의 전체 서브페이지의 콘텐츠를 일괄적으로 페이드모션 처리
+	- 해당 컨텐츠 모션을 모든 서브페이지 컴포넌트에 일일이 framer-motion적용하는 것은 비효율적
+	- Layout컴포넌트에 추가하지 않으면서 각각의 서브페이지 컴포넌트의 특정 부분에 복잡하고 일괄적인 프레임기능 적용 필요
+	- 작업1 : 서브페이지 컴포넌트 안쪽에서 컨텐츠 영역만 Wrapping처리해줄 Content.jsx 를 생성
+	- 작업2 : Context.jsx의 children으로 각 페이지의 콘텐츠를 전달해주도록 처리
+	- 작업3 : 각 서브페이지 컴포넌트에 Content컴포넌트를 Wrapper형태로 감싸서 각 페이지 전용 컨텐츠 내용 전달
+	- 작업4 : Content.jsx안쪽에 페이드인 효과의 motion컴포넌트 처리
 */
