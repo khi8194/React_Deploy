8bf29be172b9231c44df47e5f8a4ef29f9f3552b
khi8194 <khi8194@gmail.com>
2024-10-10 17:19:49 +0900

    커스텀 객체 생성시 end에 delay값 0으로 초기화

--- src/components/sub/Gallery.jsx
+++ src/components/sub/Gallery.jsx
@@ -5,17 +5,22 @@ import Modal from '../common/Modal';
 import Content from '../common/Content';
 
 export default function Gallery() {
-	console.log('Gallery Component Rendered!!');
-
 	const [Flickr, setFlickr] = useState([]);
 	const [ModalOpen, setModalOpen] = useState(false);
-	//클릭한 목록요소의 순번을 담을 상태값 생성
 	const [Index, setIndex] = useState(0);
 
+	//Gallery페이지에만 전용으로 동작할 커스텀 모션 객체 생성
+	const customMotion = {
+		init: { opacity: 0, x: 200 },
+		active: { opacity: 1, x: 0 },
+		end: { opacity: 0, x: -200 },
+		transition: { delay: 0 }
+	};
+
 	useEffect(() => {
-		const method = `flickr.people.getPhotos`;
+		const method = 'flickr.people.getPhotos';
 		const flickr_api = import.meta.env.VITE_FLICKR_API;
-		const myID = '201494903@N03';
+		const myID = '197119297@N02';
 		const num = 10;
 		const url = `https://www.flickr.com/services/rest/?method=${method}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
 
@@ -23,7 +28,6 @@ export default function Gallery() {
 			.then(data => data.json())
 			.then(json => {
 				setFlickr(json.photos.photo);
-				console.log(json);
 			});
 	}, []);
 
@@ -34,7 +38,8 @@ export default function Gallery() {
 	return (
 		<>
 			<Layout title={'GALLERY'}>
-				<Content delay={1.5}>
+				{/* Content호출시 위에서 준비한 전용 모션 정보 props로 전달 */}
+				<Content delay={1.5} customMotion={customMotion}>
 					<section className='galleryList'>
 						{Flickr.map((data, idx) => {
 							return (
