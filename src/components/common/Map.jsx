d173a699b25b57e8e94af76363293aa6ff5e5972
khi8194 <khi8194@gmail.com>
2024-10-11 13:07:05 +0900

    동적으로 지점 버튼 생성후 변경된 Index상태값에 따라 useEffect의 callback재호출 하면서 새로운 지도정보 갱신

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -1,13 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
-export default function Map() {
-	const { kakao } = window;
-	const ref_mapFrame = useRef(null);
-	const [Index, setIndex] = useState(0);
-
-    // 각 지점 정보를 참조객체로 관리
-    const ref_info = useRef([
-		{
-            /*
+/*
 			title: 'HyeHwa', //데이터 구분을 위한 타이틀
 			latlng: new kakao.maps.LatLng(37.5803593, 127.0042622), //위도,경도를 활용한 위치 인스턴스
 			markerImg: 'marker1.png', //마커이미지 경로
@@ -16,13 +7,24 @@ export default function Map() {
             markerSize: new kakao.maps.Size(232, 99), //마커사이즈 인스턴스
             arkerOffset: { offset: new kakao.maps.Point(0, 0) } //마커 위치값 인스턴스
             */
-            title: 'HyeHwa',
-			latlng: new kakao.maps.LatLng(37.5803593, 127.0042622),
+import { li } from 'framer-motion/client';
+import { useEffect, useRef, useState } from 'react';
+
+export default function Map() {
+	const { kakao } = window;
+	const ref_mapFrame = useRef(null);
+	const [Index, setIndex] = useState(0);
+
+	// 각 지점 정보를 참조객체로 관리
+	const ref_info = useRef([
+		{
+            title: 'HyeHwa', 
+			latlng: new kakao.maps.LatLng(37.5803593, 127.0042622), 
 			markerImg: 'marker1.png',
-			markerSize: new kakao.maps.Size(232, 99),
-			markerOffset: { offset: new kakao.maps.Point(116, 99) }
-        },
-        {
+            markerSize: new kakao.maps.Size(232, 99),
+            arkerOffset: { offset: new kakao.maps.Point(0, 0) } 
+		},
+		{
 			title: 'COEX',
 			latlng: new kakao.maps.LatLng(37.5094091584729, 127.0624304750884),
 			markerImg: 'marker1.png',
@@ -35,59 +37,42 @@ export default function Map() {
 			markerImg: 'marker3.png',
 			markerSize: new kakao.maps.Size(232, 99),
 			markerPos: { offset: new kakao.maps.Point(116, 99) }
-        }
+		}
 	]);
 
-    /*
-	// const inst_marker = new kakao.maps.Marker({ position: ref_info.current[0].latlng });
-    const inst_markerImg = new kakao.maps.MarkerImage(
-		// ref_info.current[0].markerImg,
-		// ref_info.current[0].markerSize,
-		// ref_info.current[0].markerOffset
-        ref_info.current[Index].markerImg,
-		ref_info.current[Index].markerSize,
-		ref_info.current[Index].markerOffset
-	);
-    */
-    //기존 참조객체명까지 매번 호출하기 번거로우므로 비구조화당을 통해 현재 Index순선 상태변화에 따라 활성화되고 있는 객체의 key값을 바로 추출
-    const { latlng, markerImg, markerSize, markerPos } = ref_info.current[Index];
+	//기존 참조객체명까지 매번 호출하기 번거로우므로 비구조화당을 통해 현재 Index순선 상태변화에 따라 활성화되고 있는 객체의 key값을 바로 추출
+	const { latlng, markerImg, markerSize, markerPos } = ref_info.current[Index];
 
-	// 마커 인스턴스 생성시 전달되는 인수의 객체에 두번째 프로퍼티로 이미지 인스턴스 연결 (이미지가 적용된 마커 생성)
-	// const inst_marker = new kakao.maps.Marker({ position: ref_info.current[0].latlng, image: inst_markerImg });
-    // const inst_marker = new kakao.maps.Marker({ position: ref_info.current[Index].latlng, image: inst_markerImg });
-    //위의 비구조화할당으로 추출한 정보값으로 마커 인스턴스 생성
+	//위의 비구조화할당으로 추출한 정보값으로 마커 인스턴스 생성
 	const inst_marker = new kakao.maps.Marker({
 		position: latlng,
 		image: new kakao.maps.MarkerImage(markerImg, markerSize, markerPos)
 	});
 
 	//컴포넌트 마운트시 한번만 지도인스턴스 생성 및 마커 인스턴스 바인딩
-    /*
-    //jsx반환되고 화면에 컴포넌트 마운트시 지도 인스턴스 생성
-    useEffect(() => {
-		// new kakao.maps.Map(ref_mapFrame.current, mapOption);
-        //지도 인스턴스 생성은 ref_mapFrame에 담겨있는 실제 돔요소를 인수로 필요로 하므로 useEffect구문안쪽에서 생성
-		//이때 두번째 인수로 위치 인스턴스 전달
-		const inst_map = new kakao.maps.Map(ref_mapFrame.current, { center: inst_position });
-		//생성된 마커인스턴의 setMap 메서드 호출시 지도 인스턴스 값 인수로 전달
-		inst_marker.setMap(inst_map);
-	}, []);
-    */
+	//Index상태값이 변경될때마다 변경된 순번 상태값으로 지도 인스턴스 다시 생성해서 화면 갱신
 	useEffect(() => {
-		// const inst_map = new kakao.maps.Map(ref_mapFrame.current, { center: ref_info.current[0].latlng });
-        /*
-        const inst_map = new kakao.maps.Map(ref_mapFrame.current, { center: ref_info.current[Index].latlng });
-        inst_marker.setMap(inst_map);
-        */
-        const inst_map = new kakao.maps.Map(ref_mapFrame.current, { center: latlng });
-	}, []);
+		const inst_map = new kakao.maps.Map(ref_mapFrame.current, { center: latlng });
+		inst_marker.setMap(inst_map);
+	}, [Index]);
 
-    return (
+	return (
 		<section className='map'>
 			<h2>Location</h2>
 
-            {/* <figure className='mapFrame'></figure> */}
 			<figure ref={ref_mapFrame} className='mapFrame'></figure>
+
+			<nav className='btnSet'>
+				<ul className='branch'>
+					{ref_info.current.map((el, idx) => (
+						//동적으로 li생성 : 클릭한 li의 순서값 idx로 Index 상태값 변경
+						// -> 컴포넌트 재랜더링되면서 변경된 순번의 정보값으로 지도화면 갱신됨
+						<li key={idx} onClick={() => setIndex(idx)}>
+							{el.title}
+						</li>
+					))}
+				</ul>
+			</nav>
 		</section>
 	);
 }
