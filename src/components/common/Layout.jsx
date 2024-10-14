477e033dda5bef3ea22146432018d756eb959b72
khi8194 <khi8194@gmail.com>
2024-10-14 12:23:37 +0900

    라우터로 페이지 컴포넌트 이동시 스크롤 위치값 상단으로 자동 초기화

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,36 +1,22 @@
 import { useLocation } from 'react-router-dom';
-// import { motion } from 'framer-motion';
-// import MaskText from './MaskText';
 import Mask from './Mask';
 import SplitText from './SplitText';
+import { useEffect } from 'react';
 
 export default function Layout({ title, children }) {
 	const { pathname } = useLocation();
 	const isDetail = pathname.includes('/youtube/');
 
+	//라우터를 통해 새로운 페이지 컴포넌트 마운트시 강제로 윈도우의 스크롤 위치값을 0으로 초기화
+	useEffect(() => {
+		window.scrollTo({ top: 0 });
+	}, []);
+
 	return (
 		<>
 			<main className={isDetail ? 'detail' : title.toLowerCase()}>
 				<SplitText delay={0.5}>{title}</SplitText>
-				<br />
-
-				{/* <MaskText delay={1} color={'#444'} style={{ fontSize: 20, fontFamily: 'arial' }}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ad?
-				</MaskText>
-				<br />
-				<MaskText delay={1.5} color={'#444'} style={{ marginBottom: 120 }}>
-					Lorem ipsum dolor
-				</MaskText> */}
-
 				<section>{children}</section>
-				{/* <motion.section
-					initial={{ opacity: 0, y: 200 }}
-					animate={{ opacity: 1, y: 0 }}
-					exit={{ opacity: 0, y: 200, transition: { delay: 0 } }}
-					// transition={{ duration: 1, delay: 1.5, ease: 'linear' }}>
-					transition={{ duration: 1, delay: 0.5, ease: 'linear' }}>
-					{children}
-				</motion.section> */}
 			</main>
 
 			<Mask duration={0.5} delay={0} style={{ position: 'fixed' }} />
