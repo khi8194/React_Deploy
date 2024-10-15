4234422d226d329efd9cafcb47244f5d8748d48a
khi8194 <khi8194@gmail.com>
2024-10-15 15:29:45 +0900

    기본 스와이퍼 옵션 적용

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -1,37 +1,30 @@
-// import { useYoutubeQuery } from '../../hooks/useYoutube';
 import { useFlickrQuery } from '../../hooks/useFlickr';
 import Pic from '../common/Pic';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 export default function Visual() {
-	// const { data } = useYoutubeQuery();
 	const { data } = useFlickrQuery({ type: 'mine' });
-	// const { data } = useFlickrQuery();
 	return (
 		<figure className='visual'>
-			{/* <div style={{ display: 'flex', gap: 40 }}> */}
-			<Swiper>
+			{/* <Swiper> */}
+			<Swiper slidesPerView={3} spaceBetween={100} loop={true}>
 				{data?.map((pic, idx) => {
 					if (idx >= 10) return null;
 					return (
-						// <article key={idx}>
-						// 	{/* <Pic src={vid.snippet.thumbnails.high.url} style={{ width: 400, height: 200 }} shadow /> */}
-						// 	<Pic
-						// 		src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg`}
-						// 		style={{ width: 400, height: 200 }}
-						// 		shadow
-						// 	/>
-						// </article>
 						<SwiperSlide key={idx}>
-							<Pic
+							{/* <Pic
 								src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg`}
 								style={{ width: '100%', height: '100%' }}
+							/> */}
+							<Pic
+								src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+								style={{ width: '100%', height: '100%' }}
+								shadow
 							/>
 						</SwiperSlide>
 					);
 				})}
-				{/* </div> */}
 			</Swiper>
 		</figure>
 	);
