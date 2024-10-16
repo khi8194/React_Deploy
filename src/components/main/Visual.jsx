bef988afd50e14ea09a0e8e215c66d39008b8e0a
khi8194 <khi8194@gmail.com>
2024-10-16 14:50:20 +0900

    breakpoints를 이용해서 타블릿 구간에서 반응형 처리

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -36,9 +36,21 @@ export default function Visual() {
 				// modules={[Autoplay]}
 				modules={[Autoplay, Pagination]}
 				pagination={{ type: 'fraction' }}
-				slidesPerView={3}
-				spaceBetween={100}
+				// slidesPerView={3}
+				// spaceBetween={100}
+				slidesPerView={1}
+				spaceBetween={0}
 				loop={true}
+				breakpoints={{
+					1000: {
+						slidesPerView: 2,
+						spaceBetween: 50
+					},
+					1400: {
+						slidesPerView: 3,
+						spaceBetween: 50
+					}
+				}}
 				centeredSlides={true}
 				onSlideChange={el => setIndex(el.realIndex)}
 				autoplay={{ delay: 2000, disableOnInteraction: true }}
