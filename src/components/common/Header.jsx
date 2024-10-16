77913d1348803f4976c84053e47c94ea6256b6b3
khi8194 <khi8194@gmail.com>
2024-10-16 15:29:29 +0900

    모바일 전용 메뉴 생성 및 스타일링

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,18 +1,14 @@
 import { FaBars, FaYoutube, FaInstagram, FaEnvelope } from 'react-icons/fa';
 import { Link, useLocation } from 'react-router-dom';
+import MobileMenu from './MobileMenu';
 
 export default function Header() {
 	const gnbArr = ['members', 'gallery', 'youtube', 'contact', 'posts'];
 	const snsArr = [FaEnvelope, FaInstagram, FaYoutube];
 
 	const { pathname } = useLocation();
-	// let currentClass = '';
-	// if (pathname === '/') currentClass = 'mainHeader';
-	// else currentClass = 'header';
 
-	return (
-		// <header className={currentClass}>
-		//메인전용 헤더에 변경되는 부분이 극히 적으므로 기존 header클래스를 베이스로 하고 메인페이지에서는 main클래스만 추가
+	/* return (
 		<header className={`header ${pathname === '/' && 'main'}`}>
 			<h1>
 				<Link to={'/'}>ALPACO</Link>
@@ -40,5 +36,39 @@ export default function Header() {
 
 			<FaBars className='btnMenuToggle' />
 		</header>
+	);
+	*/
+	return (
+		<>
+			<header className={`header ${pathname === '/' && 'main'}`}>
+				<h1>
+					<Link to={'/'}>ALPACO</Link>
+				</h1>
+
+				<nav>
+					<ul className='gnb'>
+						{gnbArr.map((data, idx) => {
+							return (
+								<li key={idx} className={pathname === '/' + data ? 'on' : ''}>
+									<Link to={'/' + data}>{data.toUpperCase()}</Link>
+								</li>
+							);
+						})}
+					</ul>
+
+					<ul className='sns'>
+						{snsArr.map((Data, idx) => (
+							<li key={idx}>
+								<Data />
+							</li>
+						))}
+					</ul>
+				</nav>
+
+				<FaBars className='btnMenuToggle' />
+			</header>
+
+			<MobileMenu />
+		</>
 	);
 }
