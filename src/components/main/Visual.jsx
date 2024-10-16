4cbbbf53fb7e3e5fc1711081a6626a11ccce5fba
khi8194 <khi8194@gmail.com>
2024-10-16 14:30:13 +0900

    자동롤링 시작 버튼 컴포넌트 추가(useSwiper로 전용 인스턴스 생성 및 활용)

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -6,17 +6,25 @@ import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import { useState } from 'react';
 
+//Swiper 컴포넌트 안쪽에서 호출할 자동롤링 시작 버튼 컴포넌트
+function BtnStart() {
+	//스와이퍼 전용 autoplay관련 메서드를 호출하기 위해서 useSwiper커스텀 훅으로 swiper 인스턴스 생성
+	const swiper = useSwiper();
+	return (
+		<button className='btnStart' onClick={() => swiper.autoplay.start()}>
+			롤링시작
+		</button>
+	);
+}
+
 export default function Visual() {
 	const [Index, setIndex] = useState();
-
 	const { data, isSuccess } = useFlickrQuery({ type: 'mine' });
 	return (
 		<figure className='visual'>
-			{/* Img titles */}
 			<div className='textBox'>
 				{/* 이미지 타이틀정보만 별로 뽑아서 Swipe 변경시마다 해당 순번의 타이틀도 같이 모션 처리 */}
 				{data?.map((el, idx) => (
-					// <h2 key={idx}>{el.title.substr(0, 30)}</h2>
 					<h2 key={idx} className={Index === idx ? 'on' : ''}>
 						{el.title.substr(0, 30)}
 					</h2>
@@ -30,18 +38,11 @@ export default function Visual() {
 				loop={true} //true일때 좌우 순환
 				centeredSlides={true} //복수개의 패널을 보이게 설정시 활성화 패널을 가운데 배치
 				onSlideChange={el => setIndex(el.realIndex)} //슬라이드 변경될때마다 현재 활성화 패널순번을 Index상태값에 저장 (loop:true)
-				autoplay={{
-					delay: 2000,
-					disableOnInteraction: true
-				}} //자동롤링시 인터벌 간격은 2초, 사용자 이벤트 발생하면 롤링 중지
+				autoplay={{ delay: 2000, disableOnInteraction: true }} //자동롤링시 인터벌 간격은 2초, 사용자 이벤트 발생하면 롤링 중지
 				//swiper 준비 완료시 파라미터로 swiper 인스턴스 전달받고 해당 인스턴스로부터 전용 자동롤링 시작 메서드를 1초있다 강제 실행 (SwiperSlide동적 생성시간 벌어줌)
-				onSwiper={swiper => {
-					console.log(swiper);
-					setTimeout(() => {
-						swiper.autoplay.start();
-					}, 1000);
-				}}>
-				{isSuccess && //리액트쿼리를 통핸 서버데이터가 모두 준비되면 그때 비로서 동적으로 SwipeSlide컴포넌트 반복생성
+				onSwiper={swiper => setTimeout(() => swiper.autoplay.start(), 1000)}>
+				{/* 데이터배열을 통해 동적생성되고 있는 Slide 컴포넌트 */}
+				{isSuccess &&
 					data.map((pic, idx) => {
 						if (idx >= 10) return null;
 						return (
@@ -56,6 +57,9 @@ export default function Visual() {
 							</SwiperSlide>
 						);
 					})}
+
+				{/* 자동롤링 시작 버튼 컴포넌트 호출 */}
+				<BtnStart />
 			</Swiper>
 		</figure>
 	);
