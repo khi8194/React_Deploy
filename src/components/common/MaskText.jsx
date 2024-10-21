239f1b7e970eab5d34dd3f1af8963ffad9059ea5
khi8194 <khi8194@gmail.com>
2024-10-21 11:27:57 +0900

    Members 페이지 컴포넌트 반응형 처리

--- src/components/common/MaskText.jsx
+++ src/components/common/MaskText.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import Mask from './Mask';
 
-export default function MaskText({ children, duration = 0.5, delay = 0, color = '#000', style }) {
+export default function MaskText({ children, className = '', duration = 0.5, delay = 0, color = '#000', style }) {
 	const frameStyle = {
 		fontSize: '1.2rem',
 		fontFamily: 'orbitron',
@@ -21,7 +21,10 @@ export default function MaskText({ children, duration = 0.5, delay = 0, color = 
 
 	return (
 		// <div style={{ ...frameStyle, ...style }}>
-		<div style={{ ...frameStyle, ...style }} className='maskText'>
+		// <div style={{ ...frameStyle, ...style }} className='maskText'>
+		// 컴포넌트에는 className이 직접적으로 적용 불가하므로 컴포넌트에 적용한 className을
+		//마치 props처럼 전달받아서 실제 컴포넌트 내부의 jsx요소에 연결
+		<div className={className} style={{ ...frameStyle, ...style }}>
 			<motion.span initial={init} animate={active} exit={end} transition={time}>
 				{children}
 			</motion.span>
