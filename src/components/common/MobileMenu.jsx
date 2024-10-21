2717e8c89a364195f0f96d8adab416342078e9bf
khi8194 <khi8194@gmail.com>
2024-10-21 11:23:43 +0900

    모바일 메뉴 스타일링

--- src/components/common/MobileMenu.jsx
+++ src/components/common/MobileMenu.jsx
@@ -2,6 +2,14 @@ import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import useThrottle from '../../hooks/useThrottle';
 import { useZustandStore } from '../../hooks/useZustand';
+import { Link } from 'react-router-dom';
+// import { FaPeopleGroup } from 'react-icons/fa6';
+// import { HiUserGroup } from 'react-icons/hi';
+import { GrGroup } from 'react-icons/gr';
+import { FaImages, FaYoutube } from 'react-icons/fa';
+// import { MdOutlineQuestionAnswere } from 'react-icons/md';
+import { RiQuestionAnswerLine } from 'react-icons/ri';
+import { RiMailSendFill } from 'react-icons/ri';
 
 export default function MobileMenu() {
 	console.log('mobileMenu');
@@ -35,7 +43,47 @@ export default function MobileMenu() {
 			animate={animate}
 			exit={exit}
 			transition={transition}>
-			MobileMenu
+			{/* MobileMenu */}
+			<h1>
+				<Link to='/'>ALPACO</Link>
+			</h1>
+
+			{/* ul>li*5>Link[to='/']{text} */}
+			<ul>
+				<li>
+					<Link to='/members'>
+						{/* <FaPeopleGroup /> */}
+						{/* <HiUserGroup /> */}
+						<GrGroup />
+						MEMBERS
+					</Link>
+				</li>
+				<li>
+					<Link to='/gallery'>
+						<FaImages />
+						GALLERY
+					</Link>
+				</li>
+				<li>
+					<Link to='/youtube'>
+						<FaYoutube />
+						YOUTUBE
+					</Link>
+				</li>
+				<li>
+					<Link to='/contact'>
+						{/* <MdOutlineQuestionAnswer /> */}
+						<RiQuestionAnswerLine />
+						CONTACT
+					</Link>
+				</li>
+				<li>
+					<Link to='/posts'>
+						<RiMailSendFill />
+						POSTS
+					</Link>
+				</li>
+			</ul>
 		</motion.aside>
 	);
 }
